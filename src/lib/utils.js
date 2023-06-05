ae4a437baec9129f6ce07f36000b3c69f40d4b38
engageintellect <jc9361@gmail.com>
2023-06-04 23:22:59 -0700

    init

---
+++ src/lib/utils.js
@@ -0,0 +1,11 @@
+// const { randomBytes } = import('node:crypto');
+import { randomBytes } from 'crypto';
+
+export const serializeNonPOJOs = (obj) => {
+	return structuredClone(obj);
+};
+
+export const generateUsername = (name) => {
+	const id = randomBytes(2).toString('hex');
+	return `${name.slice(0, 5)}${id}`;
+};
